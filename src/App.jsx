e60cece5004eb2308416d5e312f9503786b62b70
Sandip Low <sandipbkt2002@gamil.com>
2023-05-02 13:04:57 +0530

    parcel replaced with vitejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hello from './components/Hello';
 
 export default function App() {
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { server } from "../server"
 
 export default function Hello() {
